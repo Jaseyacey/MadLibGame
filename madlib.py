7054ad63b0c22971f01faaf4ef3608d375395e00
Jason Beedle <jbeedle@gmail.com>
2020-12-07 11:36:56 +0000

    first commit

---
+++ madlib.py
@@ -0,0 +1,7 @@
+noun = input("Enter an animal : ")
+noun2 = input("Enter another animal : ")
+noun3 = input("Enter a vehicle : ")
+noun4 = input("Enter a colour: ")
+
+print("The " + noun + " and the " + noun2 + " went to the sea in a " +
+      noun4 + " coloured " + noun3 + " on Tuesday")
