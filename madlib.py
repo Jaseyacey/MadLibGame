941eb8fd57215cb07d5781b8ef2dd50775424221
Jason Beedle <jbeedle@gmail.com>
2020-12-07 11:50:30 +0000

    commented the code

--- madlib.py
+++ madlib.py
@@ -1,13 +1,13 @@
+# random is randomly selecting a sentence
 import random
-
 f = open('madlibs.txt', 'r')
-
+# Reading the lines to find the word to be replaced
 madlibs = f.readlines()
-
+# This is calling the text file
 madlib = random.choice(madlibs)
-
+# Asking for user input
 noun = input("Enter a noun: ")
-
+# Replacing the word Blank with the user input which is noun
 madlib = madlib.replace("blank", noun)
 
 print(madlib)
